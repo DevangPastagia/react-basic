b1a0f6d73d44bf39f3c5d7c85512e0ad89706320
DevangPastagia <devang.pastagia@girirajdigital.com>
2025-06-11 12:02:13 +0530

    feat: first mini context project

---
+++ 08minicontext/src/components/Login.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Login() {
+  // variables
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { setUser } = useContext(UserContext);
+
+  // Methods
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser({ username, password });
+  };
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "18px" }}>
+      <h2>Login Page</h2>
+      <input
+        type="text"
+        placeholder="Enter Username"
+        value={username}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Enter Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
+
+export default Login;
